c92d69aa7ef74e606c7406384d3a5c8cdba7fef5
AydanBayram <126654872+AydanBayram@users.noreply.github.com>
2024-03-05 15:05:17 +0200

    Update Second_Project.py

--- Second_Project.py
+++ Second_Project.py
@@ -3,3 +3,23 @@ print(list1[1])
 print("[2:5]=", list1[2:5])
 print("[:4]=", list1[:4])
 print("[2:]=", list1[2:])
+thislist = [ "apple", "banana", "cherry"]
+print(thislist)
+thislist1 = [ "apple", "banana", "cherry", "apple", "cherry"]
+print(thislist1)
+print(len(thislist1))
+
+list1.append(3)
+print(list1)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+}
+today = {
+    "class ": ["dst", "network", "programming"],
+    "quantity-of-students":14,
+    "student_name": {"Aydan", "Bayram"}
+}
+print(today)
+print(today.keys())
